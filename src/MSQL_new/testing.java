2e3f2adf1b78e12b17b9f50068087ffb8d5583b2
“Younes <sghyeryounes@gmail.com>
2022-08-13 21:09:18 +0100

    Environement

---
+++ src/MSQL_new/testing.java
@@ -0,0 +1,19 @@
+
+package MSQL_new;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+	}
+	
+
+}
+
