0333438440acd6c0179c81af29a1e7d8383c4664
Younes202 <84544672+Younes202@users.noreply.github.com>
2022-08-13 21:12:54 +0100

    Preparing Sql requete

--- src/MSQL_new/testing.java
+++ src/MSQL_new/testing.java
@@ -10,10 +10,21 @@ public class testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); 
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/students_java","root","");
+			Statement st = con.createStatement(); 
+		}catch (Exception e)
+		{
+			System.out.println(e);
+		}
+
+
 	
 
 	}
 	
 
 }
+
 
